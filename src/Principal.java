bdec83993f681db504dbf5f26f17149256efe9e3
eduardonk9999 <thiago.novak7@gmail.com>
2024-02-02 13:18:39 -0300

    feat: principal

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal{
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefao";
+        System.out.println(meuFilme.nome);
+    }
+}
+
+
+
