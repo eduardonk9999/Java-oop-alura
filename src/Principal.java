687a994505a13c3f6778b2a90acac7abd0172db9
Eduardo Silva <thiago.novak7@gmail.com>
2024-03-04 22:17:18 +0000

    classes, metodos

--- src/Principal.java
+++ src/Principal.java
@@ -2,12 +2,13 @@ public class Principal{
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
         meuFilme.nome = "O poderoso chefao";
-        System.out.println(meuFilme.nome);
-        System.out.println(meuFilme.avaliacao);
-        System.out.println(meuFilme.anoDeLancamento);
+        meuFilme.anoDeLancamento = 1970;
         
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(6);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
     }
 }
-
-
-
